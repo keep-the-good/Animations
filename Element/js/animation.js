594c698c93e56c67f02b325240c959e47d743f49
Nikita <nikitos95@hotmail.com>
2024-06-07 04:07:11 +0200

    up!

--- Element/js/animation.js
+++
@@ -1,9 +0,0 @@
-anime({
-    targets: ".grid-item",
-    scale: [        
-        {value: .1, easing: 'easeOutSine', duration: 500}, 
-        {value: 1, easing: 'easeInOutQuad', duration: 1200},                
-    ],    
-    delay: anime.stagger(200, {grid: [10, 10], from: "center"}),
-    loop: true,    
-})
